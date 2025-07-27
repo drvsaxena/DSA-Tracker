8ea1873f89f57ce393bc757fe9b4b4e77d548456
drvsaxena <dhruvsaxena132@gmail.com>
2025-07-27 13:41:26 +0530

    added problems

---
+++ 5) Linked Lists/reverse.java
@@ -0,0 +1,60 @@
+public class reverse{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public Node reversell() {
+        Node prev = null;
+        Node curr = head;
+
+        while(curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+        return head;
+    }
+
+    public void print() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String args[]) {
+        reverse rv = new reverse();
+        rv.addLast(1);
+        rv.addLast(2);
+        rv.addLast(3);
+        rv.addLast(4);
+        rv.addLast(5);
+        rv.print();
+        rv.reversell();
+        rv.print();
+    }
+}
